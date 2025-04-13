43350cd1abf53f675823cc4c9891af51888648f8
Peter Etelej <peter@etelej.com>
2025-04-13 16:57:51 +0300

    Add support for long form tree command flags (#4)
    `tree` command will now support long form flags
    for example `tree --all --level 2`

--- src/main.rs
+++ src/main.rs
@@ -11,38 +11,69 @@ fn main() {
         .arg(
             Arg::new("all_files")
                 .short('a')
+                .long("all")
                 .help("All files are printed. By default tree does not print hidden files."),
         )
         .arg(
             Arg::new("level")
                 .short('L')
+                .long("level")
                 .takes_value(true)
                 .help("Max display depth of the directory tree."),
         )
         .arg(
             Arg::new("pattern")
-            .short('P')
-            .takes_value(true)
-            .help("List only those files that match the wild-card pattern. Note: you must use the -a option to also consider those files beginning with a dot '.' for matching."),
+                .short('P')
+                .long("pattern")
+                .takes_value(true)
+                .help("List only those files that match the wild-card pattern. Note: you must use the -a option to also consider those files beginning with a dot '.' for matching."),
         )
-        .arg(Arg::new("full_path").short('f').help("Prints the full path prefix for each file."),)
-        .arg(Arg::new("dir_only").short('d').help("List directories only."),)
-        .arg(Arg::new("no_indent").short('i').help("Makes tree not print the indentation lines, useful when used in conjunction with the -f option."),)
-        .arg(Arg::new("print_size").short('s').help("Print the size of each file in bytes along with the name."),)
-        .arg(Arg::new("human_readable").short('h').help("Print the size of each file but in a more human readable way, e.g. appending a size letter for kilobytes (K), megabytes (M), gigabytes (G), and so forth."),)
+        .arg(
+            Arg::new("full_path")
+                .short('f')
+                .long("full-path")
+                .help("Prints the full path prefix for each file."),
+        )
+        .arg(
+            Arg::new("dir_only")
+                .short('d')
+                .long("directories")
+                .help("List directories only."),
+        )
+        .arg(
+            Arg::new("no_indent")
+                .short('i')
+                .long("no-indent")
+                .help("Makes tree not print the indentation lines, useful when used in conjunction with the -f option."),
+        )
+        .arg(
+            Arg::new("print_size")
+                .short('s')
+                .long("size")
+                .help("Print the size of each file in bytes along with the name."),
+        )
+        .arg(
+            Arg::new("human_readable")
+                .short('h')
+                .long("human-readable")
+                .help("Print the size of each file but in a more human readable way, e.g. appending a size letter for kilobytes (K), megabytes (M), gigabytes (G), and so forth."),
+        )
         .arg(
             Arg::new("color")
                 .short('C')
+                .long("color")
                 .help("Turn colorization on using built-in color defaults."),
         )
         .arg(
             Arg::new("no_color")
                 .short('n')
+                .long("no-color")
                 .help("Turn colorization off, overridden by -C."),
         )
         .arg(
             Arg::new("ascii")
                 .short('A')
+                .long("ascii")
                 .help("Use only ASCII characters on tree display."),
         )
         .get_matches();
